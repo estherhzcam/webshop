d2ccafd5e8ddae33a8d525678315e1668dc889c6
Esther <esth2577@stud.kea.dk>
2021-02-13 19:38:03 +0100

    subcategories added main nav messed

--- js/category.js
+++ js/category.js
@@ -1,1 +1,24 @@
-const url = ""
+const url = "https://kea-alt-del.dk/t7/api/subcategories"
+
+fetch(url)
+.then(function(res) {
+    return res.json()
+})
+.then(function(data) {
+    handleSubcategories(data)
+})
+
+function handleSubcategories(data) {
+    data.forEach(showSubcat)
+}
+
+function showSubcat(subcat) {
+    console.log(subcat)
+    const template = document.querySelector("template").content;
+    const copy =template.cloneNode(true);
+    copy.querySelector(".subcategory p").textContent = subcat.subcategory;
+    const parent = document.querySelector("section");
+    parent.appendChild(copy);
+}
+
+//<div class="subcategory" id=belts><a href=""><p>Belts</p></a></div>
