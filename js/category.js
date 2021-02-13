1b36719f5e92d1c34f92a408d4ff8658599304f6
Esther <esth2577@stud.kea.dk>
2021-02-13 21:27:51 +0100

    display of letter and subcategories works

--- js/category.js
+++ js/category.js
@@ -1,3 +1,38 @@
+//create navigation
+//<li><a hrf="#letter_a">A</a></li>
+
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const letterArray = letters.split("");
+console.log(letterArray);
+letterArray.forEach(getLetter);
+
+function getLetter(letter) {
+    createNavLink(letter);
+    createSection(letter);
+}
+
+function createSection(letter) {
+const template = document.querySelector("#section-header").content;
+const copy = template.cloneNode(true);
+copy.querySelector("h1").textContent = letter;
+copy.querySelector("section").id = `letter_${letter}`;
+document.querySelector(".group").appendChild(copy);
+}
+/*  <section class="letterGroup" id=letter_a>
+        <h1>A</h1>
+        <hr>
+        <ol></ol>
+    </section> */
+
+function createNavLink(letter) {
+    const template = document.querySelector("#letters").content;
+    const copy = template.cloneNode(true);
+    copy.querySelector("a").textContent=letter;
+    copy.querySelector("a").href = `#letter_${letter}`
+    document.querySelector(".letterLinks ol").appendChild(copy);
+}
+
+
 const url = "https://kea-alt-del.dk/t7/api/subcategories"
 
 fetch(url)
@@ -14,11 +49,11 @@ function handleSubcategories(data) {
 
 function showSubcat(subcat) {
     console.log(subcat)
-    const template = document.querySelector("template").content;
-    const copy =template.cloneNode(true);
-    copy.querySelector(".subcategory p").textContent = subcat.subcategory;
-    const parent = document.querySelector("section");
-    parent.appendChild(copy);
+    const temp = document.querySelector("#letters").content;
+    const clone = temp.cloneNode(true);
+    clone.querySelector("a").textContent = subcat.subcategory;
+    const parent = document.querySelector(".group");
+    parent.appendChild(clone);
 }
 
 //<div class="subcategory" id=belts><a href=""><p>Belts</p></a></div>
